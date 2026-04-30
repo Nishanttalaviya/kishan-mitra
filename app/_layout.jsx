06d0cf4026c0300c9c6e884e2bff98ff63a4ee71
nishant talaviya <nishanttalaviya2308@gmail.com>
2026-04-30 12:45:27 +0530

    add file and udates

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,19 +1,5 @@
 import { Stack } from "expo-router";
-import { useEffect } from "react";
-import * as SplashScreen from "expo-splash-screen";
-
-// Splash auto hide bandh
-SplashScreen.preventAutoHideAsync().catch(() => {});
 
 export default function Layout() {
-  useEffect(() => {
-    const hideSplash = async () => {
-      await new Promise(resolve => setTimeout(resolve, 3000));
-      await SplashScreen.hideAsync();
-    };
-
-    hideSplash();
-  }, []);
-
   return <Stack screenOptions={{ headerShown: false }} />;
 }
--- app/index.jsx
+++ app/index.jsx
@@ -1,95 +1,34 @@
-import {
-  View,
-  Text,
-  StyleSheet,
-  Image,
-  TouchableOpacity,
-  StatusBar,
-} from "react-native";
+import { View, Image, StyleSheet } from "react-native";
+import { useEffect } from "react";
 import { router } from "expo-router";
 
-export default function Landing() {
+export default function Splash() {
+  useEffect(() => {
+    setTimeout(() => {
+      router.replace("/landing");
+    }, 3000);
+  }, []);
+
   return (
     <View style={styles.container}>
-      <StatusBar barStyle="light-content" />
-
-      {/* Background Image */}
       <Image
-        source={require("../assets/images/logo.png")} // replace with your background image
-        style={styles.background}
-  resizeMode="contain"
+        source={require("../assets/images/logo.png")}
+        style={styles.logo}
+        resizeMode="contain"
       />
-
-      {/* Dark overlay */}
-      <View style={styles.overlay} />
-
-      {/* Content */}
-      <View style={styles.content}>
-        <View>
-          <Text style={styles.title}>Welcome</Text>
-          <Text style={styles.subtitle}>
-            Login or Sign Up to Continue using our app.
-          </Text>
-        </View>
-
-       <TouchableOpacity
-  style={styles.button}
-  onPress={() => router.replace("/auth/login")}>
-  <Text style={styles.buttonText}>Get Started →</Text>
-</TouchableOpacity>
-
-      </View>
     </View>
   );
 }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
   },
-  background: {
-    width: "100%",
-    height: "100%",
-    position: "absolute",
-  },
-  overlay: {
-    ...StyleSheet.absoluteFillObject,
-    backgroundColor: "rgba(0,0,0,0.35)",
-  },
-  content: {
-    flex: 1,
-    justifyContent: "space-between",
-    padding: 30,
-  },
-  title: {
-    marginTop: 70,
-    fontSize: 36,
-    fontWeight: "700",
-    color: "#fff",
-  },
-  subtitle: {
-    marginTop: 10,
-    fontSize: 16,
-    color: "#ddd",
-    width: "85%",
-    lineHeight: 22,
-  },
-  button: {
-    backgroundColor: '#E7F2E4', 
-    width: '100%',
-    paddingVertical: 15,
-    borderRadius: 8,
-    alignItems: 'center',
-    elevation: 3, 
-    shadowColor: '#000', 
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.2,
-    shadowRadius: 2,
-    borderWidth: 1,
-    borderColor: '#4CAF50',
-  },
-  buttonText: {
-    color: '#005843',
-    fontSize: 18,
-    fontWeight: 'bold',
+  logo: {
+    width: 300,
+    height: 300,
   },
 });
